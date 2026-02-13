cf8cd56fde837bea9049d9b98b71ea44f7381035
romo05 <ro.maria5969@gmail.com>
2026-02-13 23:39:12 +0530

    Integrate Groq to complete RAG pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,45 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from schemas import QueryRequest, QueryResponse
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
 
+
+load_dotenv()
 app = FastAPI()
 
 # Load the FAISS database
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 vector_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile", 
+    temperature=0, 
+    groq_api_key=os.getenv("GROQ_API_KEY")
+)
+
+system_prompt = (
+    "You are 'Bylaw Buddy', a helpful civic assistant. "
+    "Use the following pieces of retrieved context to answer the user's question. "
+    "If you don't know the answer based on the context, say that you don't know. "
+    "Keep the answer concise and professional."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    ("human", "{input}"),
+])
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+rag_chain = create_retrieval_chain(vector_db.as_retriever(), question_answer_chain)
+
 
 @app.get('/')
 def root():
@@ -16,21 +48,22 @@ def root():
 
 @app.post('/ask', response_model=QueryResponse)
 def ask_bylaw(request: QueryRequest):
-    # Search for the top 3 most relevant chunks
-    docs = vector_db.similarity_search(request.question, k=3)
+    # Invoke RAG pipeline
+    response = rag_chain.invoke({"input": request.question})
 
-    context_text = "\n\n".join([d.page_content for d in docs])
+    # Extract text answer
+    answer_text = response["answer"]
 
     citations = [
         {
             "source": d.metadata.get("source", "Unknown"),
             "page": d.metadata.get("page", 0) + 1,
             "snippet": d.page_content[:100] + "..."
-        } for d in docs
+        } for d in response["context"]
     ]
 
     return {
-        "answer": f"I found this in the bylaws: {context_text}",
+        "answer": answer_text,
         "citations": citations
     }
 
